ebb052c8fcf4cd0e89781d5d32f12d6fe9e86519
Niraj Parab <nirajparab007@gmail.com>
2025-04-26 14:59:29 +0530

    implement better typescript such as union type, interface etc. and refactor code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,6 +9,11 @@ import {
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -17,20 +22,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) name!: string;
-  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter();
   // selectedUser = DUMMY_USERS[randomIndex];
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
   // getImage = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
   get getImage() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onUserButtonClick() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     // this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
